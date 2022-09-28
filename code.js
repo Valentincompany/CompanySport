2c8cfbb67003e90e622ef8d6415400619a7299ea
Valentin Company <valencompa@gmail.com>
2022-09-28 19:57:04 -0300

    js

--- code.js
+++ code.js
@@ -1,4 +1,43 @@
-let usuario=prompt("ingresa tu usuario");
+/*
+//variables
+
+let usuario
+let productos
+let menu
+//constantes 
+const buzos=8900;
+const camisetas=14000;
+const pelota=6500;
+const shorts=4000;
+const zapatillas=10000;
+
+//bienvenida
+usuario=prompt("ingresa tu usuario");
 alert("Bienvenido "+usuario+"!")
+
+menu = prompt("Quire saber el precio de un producto? si/no");
+prompt("que producto desea conocer?(ingrese el numero del producto) 1-buzos  2-camisetas 3-pelota  4-shorts  5-zapatillas)")
+while(menu=="si"){
+    prompt("que producto desea conocer?(ingrese el numero del producto) 1-buzos  2-camisetas 3-pelota  4-shorts  5-zapatillas")    
+    switch(productos){
+    case "1":
+    prompt("Los Buzos cuestan $"+buzos);
+    break;
+    case "2":
+    prompt("Las Camisetas cuestan $"+camisetas);
+    break;
+    case "3":
+    prompt("La Pelota cuesta $"+pelota);
+    break;
+    case "4":
+    prompt("Los Shorts cuestan $"+shorts);
+    break;
+    case "5":
+    prompt("Las zapatillas cuestan $"+zapatillas);
+    break;
+}
+}
+
+*/ 
 
 
