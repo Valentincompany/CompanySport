093485913024e63db67acdb972da00e473aebb60
Valentin Company <valencompa@gmail.com>
2022-10-14 13:24:41 -0300

    array

--- code.js
+++ code.js
@@ -2,16 +2,31 @@
 //variables
 
 let usuario;
-let productos;
 let menu;
 let cuotas=0;
-
-//constantes 
-const buzos=8900;
-const camisetas=14000;
-const pelota=6500;
-const shorts=4000;
-const zapatillas=10000;
+//array
+const productos = [
+    {
+        nombre:"buzos",
+        precio:8900,
+    },
+    {
+        nombre:"camisetas",
+        precio:14000,
+    },
+    {
+        nombre:"pelota",
+        precio:6500,
+    },
+    {
+        nombre:"shorts",
+        precio:4000,
+    },
+    {
+        nombre:"zapatillas",
+        precio:10000,
+    }
+]
 //funcion
 function login(){
     let usuario=prompt("ingrese el nombre de usuario")
@@ -20,39 +35,15 @@ function login(){
 //bienvenida
 login();
 
-menu = prompt("Quire saber el precio de un producto? si/no"); 
-while(menu!="no"){ 
-    menu=prompt("que producto desea conocer?(ingrese el numero del producto) \n1-Buzos \n2-Camisetas \n3-Pelota \n4-Shorts \n5-Zapatillas")
-    switch(menu){
-    case "1":
-    alert("Los Buzos cuestan $"+buzos);
-    cuotas=buzos/12;
-    prompt("Le recordamos que tiene 12 cuotas sin interes. El precio en 12 cuotas quedaria en: "+cuotas);
-    break;
-    case "2":
-    alert("Las Camisetas cuestan $"+camisetas);
-    cuotas=camisetas/12;
-    prompt("Le recordamos que tiene 12 cuotas sin interes. El precio en 12 cuotas quedaria en: "+cuotas);
-    break;
-    case "3":
-    alert("La Pelota cuesta $"+pelota);
-    cuotas=pelota/12;
-    prompt("Le recordamos que tiene 12 cuotas sin interes. El precio en 12 cuotas quedaria en: "+cuotas);
-    break;
-    case "4":
-    alert("Los Shorts cuestan $"+shorts);
-    cuotas=shorts/12;
-    prompt("Le recordamos que tiene 12 cuotas sin interes. El precio en 12 cuotas quedaria en: "+cuotas);
-    break;
-    case "5":
-    alert("Las zapatillas cuestan $"+zapatillas);
-    cuotas=zapatillas/12;
-    prompt("Le recordamos que tiene 12 cuotas sin interes. El precio en 12 cuotas quedaria en: "+cuotas);
-    break;
-}
-menu = prompt("Quire saber el precio de otro producto? si/no");
+//busqueda del producto
+let buscarProd = prompt("que producto desea conocer?(escriba el nombre del producto \n1-Buzos \n2-Camisetas \n3-Pelota \n4-Shorts \n5-Zapatillas")
+const encontrado = productos.find((producto) => producto.nombre == buscarProd);
+if (encontrado != undefined) {
+    console.log(encontrado);
+} else {
+    alert("Este producto no esta en stock");
 }
 
 
 
-
+//menu=prompt("que producto desea conocer?(ingrese el numero del producto) \n1-Buzos \n2-Camisetas \n3-Pelota \n4-Shorts \n5-Zapatillas")
