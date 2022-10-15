2a9fff39f743078b777197ad168496559e95cd44
Valentin Company <valencompa@gmail.com>
2022-10-15 20:25:39 -0300

    carrito

--- code.js
+++ code.js
@@ -26,7 +26,8 @@ const productos = [
         nombre:"zapatillas",
         precio:10000,
     }
-]
+];
+let carrito = []
 //funcion
 function login(){
     let usuario=prompt("ingrese el nombre de usuario")
@@ -36,14 +37,55 @@ function login(){
 login();
 
 //busqueda del producto
-let buscarProd = prompt("que producto desea conocer?(escriba el nombre del producto \n1-Buzos \n2-Camisetas \n3-Pelota \n4-Shorts \n5-Zapatillas")
-const encontrado = productos.find((producto) => producto.nombre == buscarProd);
-if (encontrado != undefined) {
-    console.log(encontrado);
-} else {
-    alert("Este producto no esta en stock");
+menu=prompt("Desea comprar algun producto(si/no)")
+while(menu!="si"&& menu!="no"){
+    alert("porfavor ingrese si o no")
+    menu=prompt("Desea comprar algun producto(si/no)")
 }
+if(menu=="si"){
+alert("Esta es nuestra lista de productos")
+let todoslosProductos = productos.map((producto)=>producto.nombre+" "+producto.precio+"$")
+alert(todoslosProductos.join(" - "))
+}else if (menu=="no"){
+    alert("Gracias por visitar nuestro sitio, hasta pronto!!")
+}
+while(menu!="no"){
+    let producto =prompt("agrega un producto al carrito")
+    let precio = 0
 
+if (producto == "buzos" || producto =="camisetas" || producto =="pelota" || producto =="shorts" || producto =="zapatillas"){
+    switch(producto){
+        case "buzos":
+        precio=8900;
+        break;
+        case "camisetas":
+        precio=14000;
+        break;
+        case "pelota":
+        precio=6500;
+        break;
+        case "shorts":
+        precio=4000;
+        break;
+        case "zapatillas":
+        precio=10000;
+        break;
+        default:
+        break;
+    }
+    let unidades = parseInt(prompt("cuantas unidades quiere llevar?"))
 
-
-//menu=prompt("que producto desea conocer?(ingrese el numero del producto) \n1-Buzos \n2-Camisetas \n3-Pelota \n4-Shorts \n5-Zapatillas")
+    carrito.push({producto, unidades, precio})
+    console.log(carrito)
+}else{
+    alert("No tenemos ese producto")
+}
+menu=prompt("desea seguir comprando? si/no")
+while(menu ==="no"){
+    alert("Gracias por la compra! hasta pronto")
+    carrito.forEach((carritoFinal)=>{
+        console.log(`producto: ${carritoFinal.producto}unidades: ${carritoFinal.unidades},total a pagar por producto ${carritoFinal.unidades*carritoFinal.precio}`)
+    })
+    break;
+}
+}
