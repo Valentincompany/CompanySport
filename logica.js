e8f29a267d596e39a65b321afa852da65fd6a0f6
Valentin Company <valencompa@gmail.com>
2022-10-24 16:30:24 -0300

    logica

---
+++ logica.js
@@ -0,0 +1,40 @@
+const productos = [
+    {
+        id:1,
+        nombre:"buzos",
+        precio:8900,
+    },
+    {
+        id:2,
+        nombre:"camisetas",
+        precio:14000,
+    },
+    {
+        id:3,
+        nombre:"pelota",
+        precio:6500,
+    },
+    {
+        id:4,
+        nombre:"shorts",
+        precio:4000,
+    },
+    {
+        id:5,
+        nombre:"zapatillas",
+        precio:10000,
+    },
+    {
+        id:6,
+        nombre:"botines",
+        precio:12000,
+    }
+];
+
+let carrito = [];
+productos.forEach((producto)=>{
+    let contenido=document.createElement("div");
+    contenido.innerHTML = `
+    
+    `
+})
