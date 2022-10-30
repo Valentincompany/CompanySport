53fde83141c6dd5a1890e87d16d298337df4cd83
Valentin Company <valencompa@gmail.com>
2022-10-30 17:55:16 -0300

    carrito

--- logica.js
+++ logica.js
@@ -1,5 +1,7 @@
 let contenedor = document.getElementById("contenidoCarrito");
+const verCarrito = document.getElementById("verCarrito");
 const carrito = []
+const modalContainer = document.getElementById("modal-container")
 
 function renderizadoProds(){
     for (const producto of productos){
@@ -8,7 +10,7 @@ function renderizadoProds(){
         <img src=${producto.img} class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${producto.nombre}</h5>
-          <p class=>${producto.precio}</p>
+          <p class=>$${producto.precio}</p>
           <button id="btn${producto.id}" class="btn btn-primary">Comprar</button>
         </div>
       </div>
@@ -26,9 +28,45 @@ renderizadoProds();
 function agregarAlCarrito(productoComprado){
   carrito.push(productoComprado);
   console.table(carrito);
-  alert("Producto: "+productoComprado.nombre+" agregado al carrito!")
 }
+verCarrito.addEventListener("click", () => {
+  modalContainer.innerHTML= "";
+  modalContainer.style.display = "flex";
+  const modalHeader = document.createElement("div");
+  modalHeader.className = "modal-header"
+  modalHeader.innerHTML =`
+  <h1 class="modal-header-tittle">carrito.</h1> 
+  `;
+  modalContainer.append(modalHeader);
 
+  const modalbutton = document.createElement("h1");
+  modalbutton.innerText = "x";
+  modalbutton.className = "modal-header-button";
+
+  modalbutton.addEventListener("click",()=> {
+    modalContainer.style.display="none"
+  });
+
+  modalHeader.append(modalbutton);
+
+  carrito.forEach((producto) => {
+  let carritoContent = document.createElement("div");
+  carritoContent.className = "modal-content";
+  carritoContent.innerHTML = `
+  <img src="${producto.img}">
+  <h3>${producto.nombre}</h3>
+  <p>$${producto.precio}</p>  
+  `;
+  modalContainer.append(carritoContent);
+});
+
+const total = carrito.reduce((acc, el)=> acc + el.precio, 0);
+
+const totalComprado = document.createElement("div")
+totalComprado.className = "total-content"
+totalComprado.innerHTML = `TOTAL A PAGAR:$${total}`;
+modalContainer.append(totalComprado);
+});
 let contenedorDest = document.getElementById("contenidoDestacados");
 
 function renderizarDest(){
@@ -38,7 +76,7 @@ function renderizarDest(){
         <img src=${destacado.img} class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${destacado.nombre}</h5>
-          <p class=>${destacado.precio}</p>
+          <p class=>$${destacado.precio}</p>
           <button id="btn${destacado.id}"class="btn btn-primary">Comprar</button>
         </div>
       </div>
@@ -56,5 +94,4 @@ renderizarDest();
 function agregarAlCarrito(productoComprado){
   carrito.push(productoComprado);
   console.table(carrito);
-  alert("Producto: "+productoComprado.nombre+" agregado al carrito!")
 }
