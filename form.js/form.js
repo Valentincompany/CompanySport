df3f4f1386e2d260e0ece47e767eb995fab5bc8a
Valentin Company <valencompa@gmail.com>
2022-11-11 19:01:53 -0300

    api-form

--- form.js/form.js
+++ form.js/form.js
@@ -1,19 +1,22 @@
+//llamar dentro de una constante al form
 const $form = document.querySelector("#form")
-
+//escuchar el evento sumbit del form
 $form.addEventListener("submit",handleSubmit)
 
  async function handleSubmit (event) {
     event.preventDefault()
+    //guardar la informacion del form
     const form = new FormData(this)
-    const response = await fetch(this.action,{
+
+    const response = await fetch(this.action, {
         method:this.method,
-        body:form,
-        Headers:{
-            "accept":"application/json"
+        body: form, 
+        headers: {
+            "Accept":"application/json"
         }
     })
     if (response.ok){
-        form.reset()
+        this.reset()
         alert("gracias por contactarnos, pronto le llegara su respuesta")
     }
-}
+}   
